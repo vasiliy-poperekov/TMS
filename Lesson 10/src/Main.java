53ae4b6ff8eae4b56dcb110ad206cb20d42bfb1b
Vasiliy Poperekov <vasya.poperekov@mail.ru>
2021-04-29 16:09:09 +0300

    ДЗ от 29.04.2021

--- Lesson 10/src/Main.java
+++ Lesson 10/src/Main.java
@@ -13,9 +13,9 @@ public class Main {
         //#3
         String[] string3 = {"Word", "Peace", "bumub", "ana"};
         int stringsNumber1 = 0;
-        for(String string : string3){
-            if(TextFormater.palChecker(string)){
-                System.out.println("Слово номер "+(stringsNumber1+1)+" попало в файл");
+        for (String string : string3) {
+            if (TextFormater.palChecker(string)) {
+                System.out.println("Слово номер " + (stringsNumber1 + 1) + " попало в файл");
             }
             stringsNumber1++;
         }
@@ -24,19 +24,19 @@ public class Main {
         //#4
         String[] strings = {"heh", "Nd fbeo pam efbecs cefmw brok", "vrk bro"};
         int stringsNumber2 = 0;
-        for(String string : strings){
-            if(TextFormater.wordsCounter(string)>=3&&TextFormater.wordsCounter(string)<=5||TextFormater.palChecker(string)){
-                System.out.println("Строка номер "+(stringsNumber2+1)+" попала в файл");
+        for (String string : strings) {
+            if (TextFormater.wordsCounter(string) >= 3 && TextFormater.wordsCounter(string) <= 5 || TextFormater.palChecker(string)) {
+                System.out.println("Строка номер " + (stringsNumber2 + 1) + " попала в файл");
             }
             stringsNumber2++;
         }
     }
 
-    public static String cutFromAtoB(String string, char startCut, char endCut){
-        return string.substring(string.indexOf(startCut)+1, string.lastIndexOf(endCut)+1);
+    public static String cutFromAtoB(String string, char startCut, char endCut) {
+        return string.substring(string.indexOf(startCut) + 1, string.lastIndexOf(endCut) + 1);
     }
 
-    public static String reverse(String string){
+    public static String reverse(String string) {
         return string.replace(string.charAt(3), string.charAt(0));
     }
 }
--- Lesson 10/src/TextFormater.java
+++ Lesson 10/src/TextFormater.java
@@ -7,18 +7,18 @@ import lombok.Setter;
 @AllArgsConstructor
 
 public class TextFormater {
-    public static int wordsCounter(String text){
+    public static int wordsCounter(String text) {
         String[] words = text.split(" ");
         return words.length;
     }
 
-    public static boolean palChecker(String text){
+    public static boolean palChecker(String text) {
         boolean flag = false;
         String[] words = text.split(" ");
-        for(String word:words){
-            for(int i=0;i<word.length();i++){
-                if(word.charAt(i)==word.charAt(word.length()-1-i)){
-                    flag=true;
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == word.charAt(word.length() - 1 - i)) {
+                    flag = true;
                 } else {
                     return false;
                 }
